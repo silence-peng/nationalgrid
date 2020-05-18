7da186bb90e45485179412ee52fdf157648f359d
MR.PENG <2151925612@qq.com>
2020-05-18 09:53:26 +0800

    前端框架提交--彭勇

---
+++ static/js/exportOrder.js
@@ -0,0 +1,24 @@
+layui.use(['laydate', 'laypage', 'layer', 'table', 'carousel', 'upload', 'element', 'slider'], function() {
+    var laydate = layui.laydate //日期
+        , laypage = layui.laypage //分页
+        , layer = layui.layer //弹层
+        , table = layui.table //表格
+        , carousel = layui.carousel //轮播
+        , upload = layui.upload //上传
+        , element = layui.element //元素操作
+        , slider = layui.slider; //滑块
+    //执行一个 table 实例
+    table.render({
+        elem: '#errorInfo'
+        ,width:880
+        ,height: 450
+        ,url: '' //数据接口
+        ,title: '错误信息表'
+        ,cols: [[ //表头
+            {field: 'sortNo', title: '序列', width:100}
+            ,{field: 'num', title: '行数', width: 100}
+            ,{field: 'orderNum', title: '运单号码', width:180}
+            ,{field: 'errorInfo', title: '错误说明', width: 500}
+        ]]
+    });
+});
