432a0e9f2215cbf7479a8e95d3bb3806fd444c10
MR.PENG <2151925612@qq.com>
2020-05-19 21:16:46 +0800

    前端框架提交--彭勇

--- static/js/receivingRecord.js
+++ static/js/receivingRecord.js
@@ -27,37 +27,35 @@ layui.use(['laydate', 'laypage', 'layer', 'table','form',"jquery"], function(){
     });
     //执行一个 table 实例
     table.render({
-        elem: '#errorInfo'
-        ,height: 500
+        elem: '#chargeInfo'
         ,page: true //开启分页
-        ,toolbar: 'default' //开启工具栏，此处显示默认图标，可以自定义模板，详见文档
-        ,totalRow: true //开启合计行
+        ,height:220
         ,url: 'test' //数据接口
         ,title: '错误信息表'
         ,cols: [[ //表头
-            {field: 'orderstatus', title: '订单状态',width:120}
-            ,{field: 'num', title: '运单号码',sort:true,width:120}
-            ,{field: 'orderNum', title: '参考编号',sort:true,width:120}
-            ,{field: 'errorInfo', title: '转单号码',sort:true,width:120}
-            ,{field: 'errorInfo', title: '打单号吗',sort:true,width:120}
-            ,{field: 'errorInfo', title: '排货状态',sort:true,width:120}
-            ,{field: 'errorInfo', title: '大胆状态',sort:true,width:120}
-            ,{field: 'errorInfo', title: '收货袋号',sort:true,width:120}
-            ,{field: 'errorInfo', title: '订单日期',sort:true,width:120}
-            ,{field: 'errorInfo', title: '录单网点',sort:true,width:120}
-            ,{field: 'errorInfo', title: '寄件网点',sort:true,width:120}
-            ,{field: 'errorInfo', title: '客户编码',sort:true,width:120}
-            ,{field: 'errorInfo', title: '客户',sort:true,width:120}
-            ,{field: 'errorInfo', title: '寄件人',sort:true,width:120}
-            ,{field: 'errorInfo', title: '寄件公司',sort:true,width:120}
-            ,{field: 'errorInfo', title: '寄件电话',sort:true,width:120}
-            ,{field: 'errorInfo', title: '寄件邮箱',sort:true,width:120}
-            ,{field: 'errorInfo', title: '指定路线',sort:true,width:120}
-            ,{field: 'errorInfo', title: '目的地编码',sort:true,width:120}
-            ,{field: 'errorInfo', title: '目的地',sort:true,width:120}
-            ,{field: 'errorInfo', title: '收件邮编',sort:true,width:120}
-            ,{field: 'errorInfo', title: '包装类型',sort:true,width:120}
-            ,{field: 'errorInfo', title: '货物类型',sort:true,width:120}
+            {field: 'orderstatus', title: '其他科目'}
+            ,{field: 'num', title: '金币'}
+            ,{field: 'orderNum', title: '币别'}
+        ]]
+    });
+    table.render({
+        elem: '#listInfo'
+        ,page: true //开启分页
+        ,height:800
+        ,url: 'test' //数据接口
+        ,title: '错误信息表'
+        ,toolbar: 'default' //开启工具栏，此处显示默认图标，可以自定义模板，详见文档
+        ,totalRow: true //开启合计行
+        ,cols: [[ //表头
+            {field: 'orderstatus', title: '件数'}
+            ,{field: 'num', title: '收费实重'}
+            ,{field: 'orderNum', title: '收费材重'}
+            ,{field: 'orderNum', title: '计抛方式'}
+            ,{field: 'orderNum', title: '收费重'}
+            ,{field: 'orderNum', title: '运费'}
+            ,{field: 'orderNum', title: '其他费用'}
+            ,{field: 'orderNum', title: '报关方式'}
+            ,{field: 'orderNum', title: '税金付款'}
         ]]
     });
 });
