ceaf30b7e17a959a482ca6322a853b2e6a055d17
zzw <1101133895@qq.com>
2020-06-05 15:15:48 +0800

    更新

--- static/js/strategy/operation/addDeliveryStrategy.js
+++ static/js/strategy/operation/addDeliveryStrategy.js
@@ -1,6 +1,26 @@
 layui.use([ 'layer','form'], function(){
     var layer = layui.layer
         ,form = layui.form;
+
+    form.on('submit(formDemo2)', function(data) {
+        var param = $("form").serialize();
+        $.ajax({
+            url:"/emp/addEmps",
+            type:"post",
+            data:param,
+            dataType:"text",
+            success:function(result){
+                if(result==true){
+                    layer.msg('新建成功！',{time:1000},function(){
+                        parent.layer.close(parent.layer.index);
+                    });
+                }else{
+                    layer.msg('新建失败！');
+                }
+            }
+        });
+        return false;
+    });
 })
 /*关闭弹出框口*/
 function x_admin_close() {
--- static/js/strategy/operation/deliveryStrategy.js
+++ static/js/strategy/operation/deliveryStrategy.js
@@ -11,19 +11,35 @@ layui.use(['laydate', 'laypage', 'layer', 'table','form'], function(){
         ,page: true //开启分页
         ,toolbar: 'default' //开启工具栏，此处显示默认图标，可以自定义模板，详见文档
         ,totalRow: true //开启合计行
-        ,url: 'http:/boycott/find' //数据接口
+        ,url: 'http://127.0.0.1:8080/boycottGoods/findBoycottGoods' //数据接口
         ,title: '错误信息表'
         ,cols: [[ //表头
             {field: 'affiliatedBranches', title: '所属网点'}
-            ,{field: 'assRouteId', title: '指定路线'}
+            ,{field: 'assRoute', title: '指定路线'}
             ,{field: 'chineseDes', title: '目的地'}
             ,{field: 'receipt', title: '收件邮编'}
-            ,{field: 'shiRouteId', title: '走货路线'}
+            ,{field: 'shiRoute', title: '走货路线'}
             ,{field: 'createPerson', title: '创建人'}
             ,{field: 'createDate', title: '创建时间'}
             ,{field: 'alterDate', title: '修改时间'}
             ,{field: 'alterPerson', title: '修改人'}
         ]]
+    });
+
+    form.on('submit(formDemo)', function(data) {
+        table.reload('testReload', {
+            page : {
+                curr : 1
+                // 重新从第 1 页开始
+            },
+            where : {
+                affiliatedBranches:$("#affiliatedBranches").val(),
+                assRoute:$("#assRoute").val(),
+                shiRoute:$("#shiRoute").val(),
+                chineseDes:$("#chineseDes").val()
+            }
+        });
+        return false;
     });
 
     /*用户-删除*/
